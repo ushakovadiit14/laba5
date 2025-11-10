16ff81dc8e6425ff0acef2eb86707476b502550e
Diana Ushakova <dielluvv707@gmail.com>
2025-11-10 08:32:48 +0500

    Create Fraction.java

---
+++ Fraction.java
@@ -0,0 +1,11 @@
+package ru.ushakova.number1;
+
+public interface Fraction  {
+    double getValue();
+    void setNumerator(int numerator);
+    void setDenominator(int denominator);
+    int getNumerator();
+    int getDenominator();
+    String toString();
+
+}
